94f4b59e7c1b745cecf9c4837787335abe82266d
Bryan Bugyi <bryanbugyi34@gmail.com>
2018-11-14 13:03:57 -0500

    Tests: fix test_make.sh so it works on TravisCI

--- tests/test_make.sh
+++ tests/test_make.sh
@@ -1,17 +1,9 @@
 EC=0
 capout=/tmp/cookie.capout
 
-make uninstall-all &> "${capout}"
-
-if [[ -f /usr/bin/cookie ]] || [[ -f /usr/bin/gutils.sh ]]; then
-    echo "[FAIL] make uninstall-all"
-    EC=1
-else
-    echo "[PASS] make uninstall-all"
-fi
+make uninstall-all &> /dev/null  # >>> SETUP
 
 make install &> "${capout}"
-
 if ! [[ -f /usr/bin/cookie && -f /usr/bin/gutils.sh ]]; then
     echo "[FAIL] make install"
     EC=1
@@ -19,10 +11,20 @@ else
     echo "[PASS] make install"
 fi
 
+make uninstall-all &> "${capout}"
+if [[ -f /usr/bin/cookie ]] || [[ -f /usr/bin/gutils.sh ]]; then
+    echo "[FAIL] make uninstall-all"
+    EC=1
+else
+    echo "[PASS] make uninstall-all"
+fi
+
 if [[ "${EC}" -ne 0 ]]; then
     printf "\n------- Captured Output -------\n"
     cat "${capout}"
 fi
+
+make install &> /dev/null  # >>> TEARDOWN
 
 rm "${capout}"
 exit "${EC}"
